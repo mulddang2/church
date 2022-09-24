6ad81d5ebeb533f202c3fd965dbbcdf4f9133105
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2022-09-24 23:23:36 +0900

    기존페이지 + 2단헤더
    기존페이지에 수정한 2단 헤더 합침 및 수정

--- last/script.js
+++ last/script.js
@@ -25,21 +25,20 @@ $(document).ready(function () {
         prevArrow: $(".gallery_prev"),
         nextArrow: $(".gallery_next"),
         responsive: [
-    
             {
                 breakpoint: 1269,
                 settings: {
                     slidesToShow: 2,
-                }
+                },
             },
-            { 
+            {
                 breakpoint: 768, //화면 너비가 768이상일 때
                 settings: {
                     slidesToShow: 1,
-                    centerMode: false
-                }
-            }
-        ] 
+                    centerMode: false,
+                },
+            },
+        ],
     });
     $(".video_all").slick({
         slidesToShow: 3,
@@ -52,22 +51,20 @@ $(document).ready(function () {
         prevArrow: $(".video_prev"),
         nextArrow: $(".video_next"),
         responsive: [
-    
             {
                 breakpoint: 1269,
                 settings: {
                     slidesToShow: 2,
-                }
+                },
             },
-            { 
+            {
                 breakpoint: 768, //화면 너비가 768이상일 때
                 settings: {
                     slidesToShow: 1,
-                    centerMode: false
-                }
-            }
-        ] 
-            
+                    centerMode: false,
+                },
+            },
+        ],
     });
     $("ul.news").slick({
         slidesToShow: 3,
@@ -76,21 +73,20 @@ $(document).ready(function () {
         prevArrow: $(".news_prev"),
         nextArrow: $(".news_next"),
         responsive: [
-    
             // {
             //     breakpoint: 1269,
             //     settings: {
             //         slidesToShow: 2,
             //     }
             // },
-            { 
+            {
                 breakpoint: 768, //화면 너비가 768이상일 때
                 settings: {
                     slidesToShow: 1,
-                    centerMode: false
-                }
-            }
-        ] 
+                    centerMode: false,
+                },
+            },
+        ],
     });
 
     //Top Button
@@ -99,16 +95,16 @@ $(document).ready(function () {
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
     var position = 0;
-    
+
     $(window).scroll(function () {
         if ($(window).scrollTop() <= 0) {
             position = 0;
         } else {
             position = $(window).scrollTop();
         }
-    
+
         position += vh(13.89);
-    
+
         $(".quick_menu").stop().animate(
             {
                 top: position,
@@ -118,21 +114,21 @@ $(document).ready(function () {
     });
 
     //Drop Menu
-    var gnb = document.querySelector(".gnb"),
-        header = document.getElementById("header");
-    
-    var headerDrop = $(".header_drop");
-    gnb.addEventListener("mouseenter", function () {
-            headerDrop.stop().animate({height: vh(56.94)}, 300);
-    });
-    header.addEventListener("mouseleave", function () {
-        headerDrop.stop().animate(
-            {
-                height: vh(0),
-            },
-            300
-        );
-    });
+    // var gnb = document.querySelector(".gnb"),
+    //     header = document.getElementById("header");
+
+    // var headerDrop = $(".header_drop");
+    // gnb.addEventListener("mouseenter", function () {
+    //         headerDrop.stop().animate({height: vh(56.94)}, 300);
+    // });
+    // header.addEventListener("mouseleave", function () {
+    //     headerDrop.stop().animate(
+    //         {
+    //             height: vh(0),
+    //         },
+    //         300
+    //     );
+    // });
 });
 
 //vh function
@@ -143,3 +139,27 @@ function vh(v) {
     );
     return (v * h) / 100;
 }
+
+/* header +버튼 */
+let hasDepthArr = document.querySelectorAll(".has-depth > a");
+let gnbDepth3Arr = document.querySelectorAll(".gnb-depth3");
+
+
+
+
+
+$(hasDepthArr).click(function (e) {
+    console.log(e);
+    // console.log(e.currentTarget.parentElement.querySelectorAll(".gnb-depth3"));
+    // let gnbDepth3Arr = document.querySelectorAll(".gnb-depth3");
+    // console.log(gnbDepth3Arr)
+    // let gnbDepth3Arr = document.querySelectorAll(".gnb-depth3.on");
+
+    // for (var i = 0; i < gnbDepth3Arr.length; i++) {
+    //     gnbDepth3Arr[i].classList.remove("on");
+    // }
+
+    // gnbDepth3Arr.forEach((element) => {
+    //     element.classList.remove("on");
+    // });
+});
--- script.js
+++ script.js
@@ -1,26 +1,4 @@
 /* header Menu */
-var header = document.querySelector("header"),
-    mainMenuList = document.querySelectorAll(".gnb-depth1 > li"),
-    subMenu = document.querySelectorAll(".gnb-depth2"),
-    headerHeight = header.offsetHeight,
-    subMenuHeight = 0;
-
-console.log(subMenu);
-
-for (var i = 0; i < subMenu.length; i++) {
-    if (subMenu[i].offsetHeight > subMenuHeight) {
-        subMenuHeight = subMenu[i].offsetHeight;
-    }
-}
-for (var i = 0; i < mainMenuList.length; i++) {
-    mainMenuList[i].addEventListener("mouseover", function () {
-        header.style.height = headerHeight + subMenuHeight +  10 +"px";
-    });
-    mainMenuList[i].addEventListener("mouseout", function () {
-        header.style.height = headerHeight + "px";
-    });
-}
-
 
 
 /*========================토글기능====================
@@ -28,17 +6,20 @@ for (var i = 0; i < mainMenuList.length; i++) {
 2. ::after 가 display:block 된다.
 */
 //3뎁스
-$('.gnb-depth2 .has-depth > a').not(".on").attr("aria-expanded","false");
-$('.gnb-depth2 .has-depth > a').on('click',function(){
-    $(this).toggleClass('on');
-    $(this).closest('.has-depth').find('.gnb-depth3').slideToggle();
-    //만약, has-depth에 on이 붙어있으면, 제거한다
+// $('.gnb-depth2 .has-depth > a').not(".on").attr("aria-expanded","false");
+// $('.gnb-depth2 .has-depth > a').on('click',function(){
+//     $(this).toggleClass('on');
+//     $(this).closest('.has-depth').find('.gnb-depth3').slideToggle();
+    
 
 
 //3뎁스 메뉴확장 접근성
-if($(this).hasClass('on')){
-        $(this).attr("aria-expanded","true");
-    }else{
-        $(this).attr("aria-expanded","false");
-    }
-});
+// if($(this).hasClass('on')){
+//         $(this).attr("aria-expanded","true");
+//     }else{
+//         $(this).attr("aria-expanded","false");
+//     }
+// });
+
+
+/* js */
